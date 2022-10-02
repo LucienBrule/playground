5b3804cd44ae15d7f0e3aca28c942093be70cc16
lucienbrule <lucien@lucienbrule.com>
2022-10-01 22:09:25 -0400

    client server render

--- client/src/main/webapp/main.tsx
+++ client/src/main/webapp/main.tsx
@@ -1,3 +1,19 @@
-console.log("Hello, world!");
+import React from "react";
+import ReactDOM from 'react-dom/client'
+import {App} from "./app/";
 
-export {}
+
+
+function init(root: HTMLElement = document.getElementById("root") as HTMLElement){
+
+    if(!root) throw new Error("No root element found");
+
+    ReactDOM.createRoot(root).render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    );
+}
+
+(window as any).init = init;
+window.addEventListener("load", () => init());
---
+++ client/src/main/webapp/views/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/*
+    The Home View is the first view that the user sees when they log in.
+    It is a simple view that displays a welcome message and a button to
+    navigate to the other views.
+
+ */
+
+export function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home View</h1>
+            <p>Click on the links above to navigate to the other views.</p>
+        </div>
+    );
+}
