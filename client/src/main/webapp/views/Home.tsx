d7f308082b32f5f625d1ade7efee33e0e582c025
lucienbrule <lucien@lucienbrule.com>
2022-10-02 14:52:54 -0400

    time sync

--- client/src/main/webapp/views/Home.tsx
+++ client/src/main/webapp/views/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 /*
     The Home View is the first view that the user sees when they log in.
@@ -8,10 +8,46 @@ import React from "react";
  */
 
 export function Home() {
+
+    const [time,setTime] = React.useState(0);
+    const [date, setDate] = React.useState("");
+    const [shouldFetchTime,setShouldFetchTime] = React.useState(true);
+    useEffect(() => {
+        const fetchTime = async () => {
+            const response = await fetch('/api/time');
+            const response_object = await response.json();
+            setTime(response_object?.time);
+        }
+
+        const fetchDate = async () => {
+            const response = await fetch('/api/date');
+            const response_object = await response.json();
+            setDate(response_object?.date);
+        }
+
+        Promise.all(
+            [
+                fetchTime(),
+                fetchDate()
+            ])
+            .then(() => setShouldFetchTime(false))
+            .catch(() => setShouldFetchTime(false));
+
+    },[shouldFetchTime]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setShouldFetchTime(true);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div>
             <h1>Welcome to the Home View</h1>
             <p>Click on the links above to navigate to the other views.</p>
+            <p>The time is: {time}</p>
+            <p>The date is: {date}</p>
         </div>
     );
 }
--- server/src/main/kotlin/io/brule/APIResource.kt
+++ server/src/main/kotlin/io/brule/APIResource.kt
@@ -11,4 +11,18 @@ class APIResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     fun hello() = "Hello from RESTEasy Reactive"
+
+    @Path("/time")
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    fun time() = mapOf("time" to System.currentTimeMillis())
+
+
+    // returns a date like 01/01/2000 00:00:00
+    @Path("/date")
+    @Produces(MediaType.APPLICATION_JSON)
+    @GET
+    fun date() = mapOf("date" to java.time.LocalDateTime.now().toString())
+
+
 }
